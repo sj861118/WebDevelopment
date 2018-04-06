85912ba7678a5df23de316b607e437b2055a0bb9
sj861118 <34262419+sj861118@users.noreply.github.com>
2018-04-06 18:55:51 +0900

    gallery example
    finish

--- Gallery/script.js
+++ Gallery/script.js
@@ -7,6 +7,28 @@ req.onreadystatechange = function(){
     for(var i = 0; i < data.length; i ++){
       var div = document.createElement("div");
       div.setAttribute("class","image");
+      div.onclick = function(){
+        /*
+        이렇게 하면 비효율 적이다.
+        if(this.getAttribute("class").indexOf("image-selected") == -1){
+          this.setAttribute("class", "image image-selected");
+        }
+        else{
+          this.setAttribute("class", "image");
+        }
+        */
+        this.classList.toggle("image-selected");
+      }
+      div.onmouseover = function(){
+        var element = this;
+        this.timerId = setTimeout(function(){
+          element.classList.add("image-magnified");
+        }, 1000);
+      }
+      div.onmouseout = function(){
+        clearTimeout(this.timerId);
+        this.classList.remove("image-magnified");
+      }
       var img = document.createElement("img");
       img.src = data[i];
       div.appendChild(img);
@@ -15,3 +37,36 @@ req.onreadystatechange = function(){
   }
 }
 req.send();
+
+function selectAll(btn){
+  var images = document.getElementsByClassName("image");
+  for (var i = 0; i < images.length; i++){
+    if(btn.value == "Select All"){
+      images[i].classList.add("image-selected");
+    }else{
+      images[i].classList.remove("image-selected");
+    }
+  }
+  if(btn.value == "Select All"){
+    btn.value = "Unselect All";
+  }
+  else{
+    btn.value = "Select All";
+  }
+}
+
+function slideShow(btn){
+  var images = document.getElementsByClassName("image");
+  var index = 0;
+  images[index].classList.add("image-magnified");
+  var intervalId = setInterval(function(){
+    images[index].classList.remove("image-magnified");
+    index++;
+    if(index < images.length){
+      images[index].classList.add("image-magnified");
+    }
+    else{
+      clearInterval(intervalId);
+    }
+  },1000);
+}
